af67dd0d586a079b64e65e187d9caa4ad4ad0499
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-27 19:24:28 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,1 +1,16 @@
+import Link from 'next/link';
+
+const NavBar = () => (
+  <nav>
+    <ul>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/results">Results</Link></li>
+      <li><Link href="/teams">Teams</Link></li>
+      <li><Link href="/candidates">Candidates</Link></li>
+      <li><Link href="/programs">Programs</Link></li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
 
