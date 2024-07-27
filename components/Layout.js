c044bfa41e947daefdfd42c2bc7c1cc9092763f9
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-27 19:23:50 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,15 @@
+import Header from './Header';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <NavBar />
+    <main>{children}</main>
+    <Footer />
+  </>
+);
+
+export default Layout;
 
