36d3a457b7dc21154c5abc66b77efbec44d20dec
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-27 19:24:58 +0530

    Update results.js

--- pages/results.js
+++ pages/results.js
@@ -1,1 +1,27 @@
+import { connectToDatabase } from '../lib/db';
+
+export default function Results({ results }) {
+  return (
+    <Layout>
+      <h1>Results</h1>
+      <ul>
+        {results.map(result => (
+          <li key={result._id}>
+            {result.program}: {result.position}, {result.grade}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+  const results = await db.collection('results').find({}).toArray();
+  return {
+    props: {
+      results: JSON.parse(JSON.stringify(results)),
+    },
+  };
+}
 
