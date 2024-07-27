6f024c4f3730d6aaffb99772568924cc754e1311
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-27 19:37:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Head from 'next/head';
-import Layout from '../components/Layout';
-
 export default function Home() {
   return (
-    <Layout>
-      <Head>
-        <title>Art Fest Management</title>
-      </Head>
-      <h1>Welcome to the Art Fest Management System</h1>
-    </Layout>
+    <div>
+      <h1>Welcome to Art Fest Management</h1>
+    </div>
   );
 }
