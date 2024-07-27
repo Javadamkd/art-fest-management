bbac80c50641735d80972cc2ad0a0338aad08862
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-27 19:25:40 +0530

    Update programs.js

--- pages/programs.js
+++ pages/programs.js
@@ -1,1 +1,27 @@
+import { connectToDatabase } from '../lib/db';
+
+export default function Programs({ programs }) {
+  return (
+    <Layout>
+      <h1>Programs</h1>
+      <ul>
+        {programs.map(program => (
+          <li key={program._id}>
+            {program.name} - {program.section}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+  const programs = await db.collection('programs').find({}).toArray();
+  return {
+    props: {
+      programs: JSON.parse(JSON.stringify(programs)),
+    },
+  };
+}
 
