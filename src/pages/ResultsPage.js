f98e21210d2b751dee75bf4396583a08b970dc05
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:29:22 +0530

    Update ResultsPage.js

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function ResultsPage() {
+  return (
+    <div>
+      <h1>Results</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default ResultsPage;
 
