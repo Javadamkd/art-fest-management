5da865fb92e3de02da4f16b4a9d9eb080e6085c9
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:29:39 +0530

    Update TeamsPage.js

--- src/pages/TeamsPage.js
+++ src/pages/TeamsPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function TeamsPage() {
+  return (
+    <div>
+      <h1>Teams</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default TeamsPage;
 
