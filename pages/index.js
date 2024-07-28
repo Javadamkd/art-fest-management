f3a8354057df6c327a7068c84a01a3046f7781e2
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 22:23:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ export default function Home() {
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome to Art Fest Management</h1>
       <div className={styles.links}>
-        <Link href="/candidates"><a className={styles.link}>Candidates</a></Link>
-        <Link href="/rankAndTop"><a className={styles.link}>Rank & Top</a></Link>
-        <Link href="/search"><a className={styles.link}>Search</a></Link>
-        <Link href="/programs"><a className={styles.link}>Programs</a></Link>
-        <Link href="/results"><a className={styles.link}>Results</a></Link>
-        <Link href="/teams"><a className={styles.link}>Teams</a></Link>
+        <Link href="/candidates" className={styles.link}>Candidates</Link>
+        <Link href="/rankAndTop" className={styles.link}>Rank & Top</Link>
+        <Link href="/search" className={styles.link}>Search</Link>
+        <Link href="/programs" className={styles.link}>Programs</Link>
+        <Link href="/results" className={styles.link}>Results</Link>
+        <Link href="/teams" className={styles.link}>Teams</Link>
       </div>
     </div>
   );
