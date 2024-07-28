3276497f8095b8dd4c4b1e15d232e380ab4677fb
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:22:32 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <Link to="/candidates">Candidates</Link>
+      <Link to="/programs">Programs</Link>
+      <Link to="/results">Results</Link>
+      <Link to="/teams">Teams</Link>
+    </nav>
+  );
+}
+
+export default Navigation;
 
