628fea6fbe8821c020e43053c8ce9f10bc89bc9f
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:27:59 +0530

    Update ProgramsPage.js

--- src/pages/ProgramsPage.js
+++ src/pages/ProgramsPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function ProgramsPage() {
+  return (
+    <div>
+      <h1>Programs</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default ProgramsPage;
 
