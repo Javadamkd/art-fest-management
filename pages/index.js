fc356d7713f804983dc1df9c4bba1deb7d8676da
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 07:15:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to Art Fest Management</h1>
+      <nav>
+        <ul>
+          <li><Link href="/candidates">Candidates</Link></li>
+          <li><Link href="/programs">Programs</Link></li>
+          <li><Link href="/results">Results</Link></li>
+          <li><Link href="/teams">Teams</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
