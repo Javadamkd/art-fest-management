2ea2455cacfcdb29523c98d38a749cc4554485fe
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:27:43 +0530

    Update CandidatesPage.js

--- src/pages/CandidatesPage.js
+++ src/pages/CandidatesPage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function CandidatesPage() {
+  return (
+    <div>
+      <h1>Candidates</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default CandidatesPage;
 
