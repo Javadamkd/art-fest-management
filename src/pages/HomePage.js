4b7570468bea92f4721954b56ffa4a3fde88e089
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 11:26:54 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li><Link to="/candidates">Candidates</Link></li>
+        <li><Link to="/programs">Programs</Link></li>
+        <li><Link to="/results">Results</Link></li>
+        <li><Link to="/teams">Teams</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
 
