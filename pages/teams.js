462f97686b466c93997eaf84ffaaa5cc2fae5151
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 07:16:53 +0530

    Update teams.js

--- pages/teams.js
+++ pages/teams.js
@@ -1,27 +1,17 @@
-import { connectToDatabase } from '../lib/db';
+import Link from 'next/link';
 
-export default function Teams({ teams }) {
+export default function Teams() {
   return (
-    <Layout>
-      <h1>Teams</h1>
-      <ul>
-        {teams.map(team => (
-          <li key={team._id}>
-            {team.name} - {team.totalScore} points
-          </li>
-        ))}
-      </ul>
-    </Layout>
+    <div>
+      <h1>Teams Page</h1>
+      <nav>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/candidates">Candidates</Link></li>
+          <li><Link href="/programs">Programs</Link></li>
+          <li><Link href="/results">Results</Link></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
-
-export async function getServerSideProps() {
-  const { db } = await connectToDatabase();
-  const teams = await db.collection('teams').find({}).toArray();
-  return {
-    props: {
-      teams: JSON.parse(JSON.stringify(teams)),
-    },
-  };
-}
-
