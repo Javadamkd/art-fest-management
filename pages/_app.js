cc97a081b9903ec4bb08edb71f76a5fca1780df8
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-28 07:46:17 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
-// pages/_app.js
-import React from 'react';
-import App from 'next/app';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CandidatesPage from './pages/CandidatesPage';
+import ProgramsPage from './pages/ProgramsPage';
+import ResultsPage from './pages/ResultsPage';
+import TeamsPage from './pages/TeamsPage';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
-  }
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/candidates" component={CandidatesPage} />
+        <Route path="/programs" component={ProgramsPage} />
+        <Route path="/results" component={ResultsPage} />
+        <Route path="/teams" component={TeamsPage} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default MyApp;
+export default App;
