b19781242ea2bc9dbf555c4905e132fc99763f83
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 23:17:03 +0530

    Update programs.js

--- pages/programs.js
+++ pages/programs.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import Layout from '../components/Layout';
 
-const Programs = () => {
+export default function Program() {
   return (
-    <div>
-      <h1>Programs</h1>
-      {/* Add your Programs page content here */}
-    </div>
+    <Layout>
+      <h1>Program</h1>
+      <form>
+        <label>
+          Code:
+          <input type="text" name="code" />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Section:
+          <input type="text" name="section" />
+        </label>
+        <label>
+          Score Type (Position, Grade):
+          <input type="text" name="scoreType" />
+        </label>
+        <label>
+          Status (Published, To be Published, Not entered, Entered...):
+          <input type="text" name="status" />
+        </label>
+        <label>
+          Published Time:
+          <input type="datetime-local" name="publishedTime" />
+        </label>
+        <button type="submit">Add Program</button>
+      </form>
+    </Layout>
   );
-};
-
-export default Programs;
+}
