bd108cd1f4a487242bcdcd5d64f8a2816722c0af
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 17:31:57 +0530

    Update rank-top.js

--- pages/rank-top.js
+++ pages/rank-top.js
@@ -1,50 +1,13 @@
 // pages/rank-top.js
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function RankTop() {
   return (
-    <div className="container">
-      <Sidebar />
-      <div className="content">
-        <h1>Rank & Top</h1>
-        <div className="card">
-          <p>Ranking and top candidates...</p>
-        </div>
+    <Layout>
+      <h1>Rank & Top</h1>
+      <div className="card">
+        <p>Ranking and top candidates...</p>
       </div>
-    </div>
-  );
-}
-
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li>
-          <Link href="/candidates">
-            Candidates
-          </Link>
-        </li>
-        <li>
-          <Link href="/teams">
-            Teams
-          </Link>
-        </li>
-        <li>
-          <Link href="/basic">
-            Basic
-          </Link>
-        </li>
-        <li>
-          <Link href="/rank-top">
-            Rank & Top
-          </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
+    </Layout>
   );
 }
