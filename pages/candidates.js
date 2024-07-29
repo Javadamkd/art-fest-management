9cefdb7cc1a13e9e91aecfbd3d37b64b7ad2ad85
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 17:32:10 +0530

    Update candidates.js

--- pages/candidates.js
+++ pages/candidates.js
@@ -1,50 +1,13 @@
 // pages/candidates.js
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function Candidates() {
   return (
-    <div className="container">
-      <Sidebar />
-      <div className="content">
-        <h1>Candidates</h1>
-        <div className="card">
-          <p>List of candidates...</p>
-        </div>
+    <Layout>
+      <h1>Candidates</h1>
+      <div className="card">
+        <p>List of candidates...</p>
       </div>
-    </div>
-  );
-}
-
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li>
-          <Link href="/candidates">
-            Candidates
-          </Link>
-        </li>
-        <li>
-          <Link href="/teams">
-            Teams
-          </Link>
-        </li>
-        <li>
-          <Link href="/basic">
-            Basic
-          </Link>
-        </li>
-        <li>
-          <Link href="/rank-top">
-            Rank & Top
-          </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
+    </Layout>
   );
 }
