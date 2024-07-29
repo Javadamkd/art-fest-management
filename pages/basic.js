cd9796d87d8a44e88e96bc1b264b04aacd51d075
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 17:32:35 +0530

    Update basic.js

--- pages/basic.js
+++ pages/basic.js
@@ -1,50 +1,13 @@
 // pages/basic.js
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function Basic() {
   return (
-    <div className="container">
-      <Sidebar />
-      <div className="content">
-        <h1>Basic</h1>
-        <div className="card">
-          <p>Basic information...</p>
-        </div>
+    <Layout>
+      <h1>Basic</h1>
+      <div className="card">
+        <p>Basic information...</p>
       </div>
-    </div>
-  );
-}
-
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li>
-          <Link href="/candidates">
-            Candidates
-          </Link>
-        </li>
-        <li>
-          <Link href="/teams">
-            Teams
-          </Link>
-        </li>
-        <li>
-          <Link href="/basic">
-            Basic
-          </Link>
-        </li>
-        <li>
-          <Link href="/rank-top">
-            Rank & Top
-          </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
+    </Layout>
   );
 }
