4045bf4560f0d199b7b414e8cb721d2981f9b47e
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 18:00:08 +0530

    Update basic.js

--- pages/basic.js
+++ pages/basic.js
@@ -1,12 +1,31 @@
 // pages/basic.js
+import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function Basic() {
   return (
     <Layout>
-      <h1>Basic</h1>
       <div className="card">
-        <p>Basic information...</p>
+        <h1>Basic</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="programType">Program Type:</label>
+            <select id="programType" name="programType">
+              <option value="individual">Individual</option>
+              <option value="sharedByTwo">Shared by Two</option>
+              <option value="master">Master</option>
+              <option value="group">Group</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="positionScore">Position Score Scale:</label>
+            <input type="number" id="positionScore" name="positionScore" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="gradeScore">Grade Score Scale:</label>
+            <input type="number" id="gradeScore" name="gradeScore" />
+          </div>
+        </form>
       </div>
     </Layout>
   );
