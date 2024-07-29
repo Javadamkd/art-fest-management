e8f8ad1aa4304243b8ef19bb998987e64e87079d
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 18:16:10 +0530

    Update candidates.js

--- pages/candidates.js
+++ pages/candidates.js
@@ -1,27 +1,11 @@
 // pages/candidates.js
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function Candidates() {
   return (
     <Layout>
-      <div className="card">
-        <h1>Candidates</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Chest No</th>
-              <th>Name</th>
-              <th>Team Code</th>
-              <th>Section</th>
-              <th>Class</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Candidate rows will go here */}
-          </tbody>
-        </table>
-      </div>
+      <h1>Candidates</h1>
+      <p>Content for Candidates page.</p>
     </Layout>
   );
 }
