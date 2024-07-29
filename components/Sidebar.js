2ebe31579a45fb2e8b143402d1e4b0dcf67b9967
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 23:21:32 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,36 +1,20 @@
 // components/Sidebar.js
 import Link from 'next/link';
+import styles from './Layout.module.css';
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <ul>
-      <li>
-        <Link href="/candidates">
-          Candidates
-        </Link>
-      </li>
-      <li>
-        <Link href="/teams">
-          Teams
-        </Link>
-      </li>
-      <li>
-        <Link href="/basic">
-          Basic
-        </Link>
-      </li>
-      <li>
-        <Link href="/rank-top">
-          Rank & Top
-        </Link>
-      </li>
-      <li>
-        <Link href="/search">
-          Search
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
-
-export default Sidebar;
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <h2>Art Fest Management</h2>
+      <ul>
+        <li><Link href="/basic">Basic</Link></li>
+        <li><Link href="/teams">Teams</Link></li>
+        <li><Link href="/candidates">Candidates</Link></li>
+        <li><Link href="/programs">Programs</Link></li>
+        <li><Link href="/result">Result</Link></li>
+        <li><Link href="/rank-top">Rank & Top</Link></li>
+        <li><Link href="/search">Search</Link></li>
+      </ul>
+    </div>
+  );
+}
