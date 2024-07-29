9499066493de8984800ee720f75d1c867277e1f9
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 18:15:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,10 @@
 // pages/index.js
-import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
-      <div className="card">
-        <h1>Welcome to Art Fest Management</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/candidates" legacyBehavior>
-                <a>Candidates</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/teams" legacyBehavior>
-                <a>Teams</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/basic" legacyBehavior>
-                <a>Basic</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/rank-top" legacyBehavior>
-                <a>Rank & Top</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/search" legacyBehavior>
-                <a>Search</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      {/* Keep this area blank */}
     </Layout>
   );
 }
