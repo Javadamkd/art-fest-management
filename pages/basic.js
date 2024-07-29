129b266e980eb0327fee59e814608d8ab0399fda
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 23:16:26 +0530

    Update basic.js

--- pages/basic.js
+++ pages/basic.js
@@ -6,12 +6,24 @@ export default function Basic() {
       <h1>Basic</h1>
       <form>
         <label>
-          Basic Info:
-          <input type="text" name="basicInfo" />
+          Fest Details:
+          <input type="text" name="festDetails" />
+        </label>
+        <label>
+          Section List:
+          <input type="text" name="sectionList" />
+          <button type="button">Add Section</button>
+        </label>
+        <label>
+          Position Score Scale (1st, 2nd, 3rd):
+          <input type="text" name="positionScoreScale" />
+        </label>
+        <label>
+          Grade Score Scale (A, B, C):
+          <input type="text" name="gradeScoreScale" />
         </label>
         <button type="submit">Submit</button>
       </form>
-      {/* Add more form fields and functionalities as needed */}
     </Layout>
   );
 }
