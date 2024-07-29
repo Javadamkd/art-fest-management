f9e13aa7dd1e42a7418fb5905d273a7b8d796518
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 17:32:47 +0530

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,50 +1,13 @@
 // pages/search.js
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function Search() {
   return (
-    <div className="container">
-      <Sidebar />
-      <div className="content">
-        <h1>Search</h1>
-        <div className="card">
-          <p>Search for candidates, teams, etc...</p>
-        </div>
+    <Layout>
+      <h1>Search</h1>
+      <div className="card">
+        <p>Search for candidates, teams, etc...</p>
       </div>
-    </div>
-  );
-}
-
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <ul>
-        <li>
-          <Link href="/candidates">
-            Candidates
-          </Link>
-        </li>
-        <li>
-          <Link href="/teams">
-            Teams
-          </Link>
-        </li>
-        <li>
-          <Link href="/basic">
-            Basic
-          </Link>
-        </li>
-        <li>
-          <Link href="/rank-top">
-            Rank & Top
-          </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            Search
-          </Link>
-        </li>
-      </ul>
-    </div>
+    </Layout>
   );
 }
