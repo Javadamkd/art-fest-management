765f58230e5d86fac57d395ed222d47c2e30cac1
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 11:44:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,75 @@
+// pages/index.js
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container">
-      <div className="sidebar">
+      <Sidebar />
+      <div className="content">
         <h1>Art Fest Management</h1>
+        <h2>Art Fest Management</h2>
         <ul>
           <li>
-            <Link href="/candidates" legacyBehavior>
-              <a>Candidates</a>
+            <Link href="/candidates">
+              Candidates
             </Link>
           </li>
           <li>
-            <Link href="/teams" legacyBehavior>
-              <a>Teams</a>
+            <Link href="/teams">
+              Teams
             </Link>
           </li>
           <li>
-            <Link href="/basic" legacyBehavior>
-              <a>Basic</a>
+            <Link href="/basic">
+              Basic
             </Link>
           </li>
           <li>
-            <Link href="/rank-top" legacyBehavior>
-              <a>Rank & Top</a>
+            <Link href="/rank-top">
+              Rank & Top
             </Link>
           </li>
           <li>
-            <Link href="/search" legacyBehavior>
-              <a>Search</a>
+            <Link href="/search">
+              Search
             </Link>
           </li>
         </ul>
       </div>
-      <div className="content">
-        <h1>Art Fest Management</h1>
-      </div>
+    </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <Link href="/candidates">
+            Candidates
+          </Link>
+        </li>
+        <li>
+          <Link href="/teams">
+            Teams
+          </Link>
+        </li>
+        <li>
+          <Link href="/basic">
+            Basic
+          </Link>
+        </li>
+        <li>
+          <Link href="/rank-top">
+            Rank & Top
+          </Link>
+        </li>
+        <li>
+          <Link href="/search">
+            Search
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
