a7cf39af2e0331abbc79adcf28ecda555ea952b3
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 23:11:01 +0530

    Update teams.js

--- pages/teams.js
+++ pages/teams.js
@@ -1,11 +1,17 @@
-// pages/teams.js
 import Layout from '../components/Layout';
 
 export default function Teams() {
   return (
     <Layout>
       <h1>Teams</h1>
-      <p>Content for Teams page.</p>
+      <form>
+        <label>
+          Team Name:
+          <input type="text" name="teamName" />
+        </label>
+        <button type="submit">Add Team</button>
+      </form>
+      {/* Add more form fields and functionalities as needed */}
     </Layout>
   );
 }
