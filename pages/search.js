1cf8f3c9e34550555677c28af50f8093c1a4a77c
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-29 23:11:48 +0530

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,17 @@
-// pages/search.js
 import Layout from '../components/Layout';
 
 export default function Search() {
   return (
     <Layout>
       <h1>Search</h1>
-      <p>Content for Search page.</p>
+      <form>
+        <label>
+          Search Query:
+          <input type="text" name="query" />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+      {/* Add more form fields and functionalities as needed */}
     </Layout>
   );
 }
