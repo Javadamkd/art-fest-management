c1e15a3632f7f985513ab2ee5ad34aa4f93d1e7c
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-07-31 20:02:57 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,19 +1,32 @@
 // components/Sidebar.js
 import Link from 'next/link';
-import styles from './Layout.module.css';
+import styles from '../styles/Layout.module.css'; // Corrected import path
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <h2>Art Fest Management</h2>
       <ul>
-        <li><Link href="/basic">Basic</Link></li>
-        <li><Link href="/teams">Teams</Link></li>
-        <li><Link href="/candidates">Candidates</Link></li>
-        <li><Link href="/programs">Programs</Link></li>
-        <li><Link href="/result">Result</Link></li>
-        <li><Link href="/rank-top">Rank & Top</Link></li>
-        <li><Link href="/search">Search</Link></li>
+        <li>
+          <Link href="/candidates">Candidates</Link>
+        </li>
+        <li>
+          <Link href="/teams">Teams</Link>
+        </li>
+        <li>
+          <Link href="/basic">Basic</Link>
+        </li>
+        <li>
+          <Link href="/programs">Programs</Link>
+        </li>
+        <li>
+          <Link href="/result">Result</Link>
+        </li>
+        <li>
+          <Link href="/rank-top">Rank & Top</Link>
+        </li>
+        <li>
+          <Link href="/search">Search</Link>
+        </li>
       </ul>
     </div>
   );
