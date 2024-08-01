232d7606fac30288450823b559629d11d60fab6a
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-01 15:54:18 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,3 @@
 export default function Sidebar() {
   // component code
-}import Link from 'next/link';
-
-const Sidebar = () => {
-  return (
-    <nav>
-      <Link href="/teams">Teams</Link>
-      <Link href="/programs">Programs</Link>
-      <Link href="/candidates">Candidates</Link>
-    </nav>
-  );
-};
+}
