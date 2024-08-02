c2c895880516339140ca692e783c936866e99be1
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-02 20:23:41 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,38 @@
 // components/Layout.js
-import { useState } from 'react';
-import Sidebar from './Sidebar';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import styles from './layout.module.css';
 
 const Layout = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+    setIsOpen(!isOpen);
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <button className={styles.menuButton} onClick={toggleSidebar}>
-        &#9776; {/* Hamburger icon */}
+        &#9776;
       </button>
-      <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
-        <Sidebar />
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+        <h2>Menu</h2>
+        <ul>
+          <li>
+            <Link href="/"><a>Home</a></Link>
+          </li>
+          <li>
+            <Link href="/teams"><a>Teams</a></Link>
+          </li>
+          <li>
+            <Link href="/candidates"><a>Candidates</a></Link>
+          </li>
+          <li>
+            <Link href="/programs"><a>Programs</a></Link>
+          </li>
+        </ul>
       </div>
-      <div className={styles.content}>
+      <div className={`${styles.content} ${isOpen ? styles.shifted : ''}`}>
         {children}
       </div>
     </div>
