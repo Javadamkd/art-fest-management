2b9f85043c2f6fc0cf22706ff64d4b28586568b3
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-02 20:18:32 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,21 @@
 // components/Layout.js
+import { useState } from 'react';
 import Sidebar from './Sidebar';
 import styles from './layout.module.css';
 
 const Layout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>
+      <button className={styles.menuButton} onClick={toggleSidebar}>
+        &#9776; {/* Hamburger icon */}
+      </button>
+      <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
         <Sidebar />
       </div>
       <div className={styles.content}>
