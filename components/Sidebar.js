308ef148cecb307bedbae4ad3462c3bba507a620
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-02 20:06:14 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,3 +1,36 @@
-export default function Sidebar() {
-  // component code
-}
+// components/Sidebar.js
+import Link from 'next/link';
+import styles from './sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <h2>Menu</h2>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/teams">
+            <a>Teams</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/candidates">
+            <a>Candidates</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/programs">
+            <a>Programs</a>
+          </Link>
+        </li>
+        {/* Add more links as needed */}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
