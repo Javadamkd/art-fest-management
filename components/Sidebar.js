97743d133edd638657b8ae8f790243bb88964744
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-05 05:48:48 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,23 +7,23 @@ const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.menuItem}>
-        <Link href="/">
-          <a className={`${styles.menuLink} ${styles.home}`}>Home</a>
+        <Link href="/" passHref>
+          <div className={`${styles.menuLink} ${styles.home}`}>Home</div>
         </Link>
       </div>
       <div className={styles.menuItem}>
-        <Link href="/teams">
-          <a className={`${styles.menuLink} ${styles.teams}`}>Teams</a>
+        <Link href="/teams" passHref>
+          <div className={`${styles.menuLink} ${styles.teams}`}>Teams</div>
         </Link>
       </div>
       <div className={styles.menuItem}>
-        <Link href="/candidates">
-          <a className={`${styles.menuLink} ${styles.candidates}`}>Candidates</a>
+        <Link href="/candidates" passHref>
+          <div className={`${styles.menuLink} ${styles.candidates}`}>Candidates</div>
         </Link>
       </div>
       <div className={styles.menuItem}>
-        <Link href="/programs">
-          <a className={`${styles.menuLink} ${styles.programs}`}>Programs</a>
+        <Link href="/programs" passHref>
+          <div className={`${styles.menuLink} ${styles.programs}`}>Programs</div>
         </Link>
       </div>
     </div>
