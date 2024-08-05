6f3bdcc7708fda715bd3295ddb87eadf87b3ed0c
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-05 21:23:55 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,34 +1,25 @@
-// components/Layout.js (or wherever your navigation is defined)
 import Link from 'next/link';
-import styles from './Layout.module.css';
 
-const Layout = ({ children }) => {
+function Menu() {
   return (
-    <div className={styles.container}>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/"><a>Home</a></Link>
-          </li>
-          <li>
-            <Link href="/teams"><a>Teams</a></Link>
-          </li>
-          <li>
-            <Link href="/candidates"><a>Candidates</a></Link>
-          </li>
-          <li>
-            <Link href="/programs"><a>Programs</a></Link>
-          </li>
-          <li>
-            <Link href="/results"><a>Results</a></Link> {/* Add this line */}
-          </li>
-        </ul>
-      </nav>
-      <main>
-        {children}
-      </main>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/teams">Teams</Link>
+        </li>
+        <li>
+          <Link href="/candidates">Candidates</Link>
+        </li>
+        <li>
+          <Link href="/programs">Programs</Link>
+        </li>
+        <li>
+          <Link href="/results">Results</Link>
+        </li>
+      </ul>
+    </nav>
   );
-};
-
-export default Layout;
+}
