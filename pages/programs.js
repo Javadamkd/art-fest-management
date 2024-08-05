90d05a2f5d96a66fe29511ea1eb4576e12726a93
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-05 05:58:42 +0530

    Update programs.js

--- pages/programs.js
+++ pages/programs.js
@@ -1,26 +1,19 @@
 // pages/programs.js
-import React from 'react';
-import styles from './programs.module.css';
+import styles from './Programs.module.css';
 
 const Programs = () => {
   return (
     <div className={styles.formContainer}>
-      <form className={styles.form}>
-        <label htmlFor="programName" className={styles.label}>Program Name:</label>
-        <input type="text" id="programName" name="programName" required className={styles.input} />
-
-        <label htmlFor="date" className={styles.label}>Date:</label>
-        <input type="text" id="date" name="date" required className={styles.input} />
-
-        <label htmlFor="time" className={styles.label}>Time:</label>
-        <input type="text" id="time" name="time" required className={styles.input} />
-
-        <label htmlFor="venue" className={styles.label}>Venue:</label>
-        <input type="text" id="venue" name="venue" required className={styles.input} />
-
-        <div className={styles.buttonContainer}>
-          <button type="submit" className={styles.button}>Submit</button>
-        </div>
+      <form className={styles.programForm}>
+        <label>Program Name:</label>
+        <input type="text" />
+        <label>Date:</label>
+        <input type="text" />
+        <label>Time:</label>
+        <input type="text" />
+        <label>Venue:</label>
+        <input type="text" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
