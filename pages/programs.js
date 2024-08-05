e6b03a8aa1fc83459018ff8d9e1c43d21723f2ce
Javadamkd <85800198+Javadamkd@users.noreply.github.com>
2024-08-05 15:50:36 +0530

    Update programs.js

--- pages/programs.js
+++ pages/programs.js
@@ -1,18 +1,49 @@
 // pages/programs.js
+import { useState } from 'react';
 import styles from './Programs.module.css';
 
 const Programs = () => {
+  const [section, setSection] = useState('');
+  const [scoreType, setScoreType] = useState('');
+  const [gradeType, setGradeType] = useState('');
+
   return (
     <div className={styles.formContainer}>
       <form className={styles.programForm}>
-        <label>Program Name:</label>
-        <input type="text" />
-        <label>Date:</label>
-        <input type="text" />
-        <label>Time:</label>
-        <input type="text" />
-        <label>Venue:</label>
-        <input type="text" />
+        <label>Code:</label>
+        <input type="text" name="code" placeholder="Enter Code" />
+        
+        <label>Name:</label>
+        <input type="text" name="name" placeholder="Enter Name" />
+        
+        <label>Section:</label>
+        <select name="section" value={section} onChange={(e) => setSection(e.target.value)}>
+          <option value="" disabled>Select Section</option>
+          <option value="Aliya">Aliya</option>
+          <option value="Thaniya">Thaniya</option>
+          <option value="Thanawiyya">Thanawiyya</option>
+          <option value="Bidaya">Bidaya</option>
+          <option value="Uoola">Uoola</option>
+        </select>
+        
+        <label>Score Type:</label>
+        <select name="scoreType" value={scoreType} onChange={(e) => setScoreType(e.target.value)}>
+          <option value="" disabled>Select Score Type</option>
+          <option value="Individual">Individual</option>
+          <option value="Group">Group</option>
+          <option value="Shared by two">Shared by two</option>
+          <option value="Master">Master</option>
+        </select>
+        
+        <label>Grade Type:</label>
+        <select name="gradeType" value={gradeType} onChange={(e) => setGradeType(e.target.value)}>
+          <option value="" disabled>Select Grade Type</option>
+          <option value="Individual">Individual</option>
+          <option value="Group">Group</option>
+          <option value="Shared by two">Shared by two</option>
+          <option value="Master">Master</option>
+        </select>
+        
         <button type="submit">Submit</button>
       </form>
     </div>
